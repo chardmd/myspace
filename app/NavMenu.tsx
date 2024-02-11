bcdf1f4d4faa66fe04da3dff0dddab8661ffc37c
Richard Dimalanta <4786193+chardmd@users.noreply.github.com>
2024-02-12 07:25:02 +1100

    sign in button

--- app/NavMenu.tsx
+++ app/NavMenu.tsx
@@ -1,11 +1,13 @@
-import Link from 'next/link';
-import styles from './NavMenu.module.css';
-import Image from 'next/image';
+import Link from "next/link";
+import styles from "./NavMenu.module.css";
+import Image from "next/image";
+import { SignInButton, SignOutButton } from "@/components/buttons";
+import AuthCheck from "@/components/AuthCheck";
 
 export default function NavMenu() {
   return (
     <nav className={styles.nav}>
-      <Link href={'/'}>
+      <Link href={"/"}>
         <Image
           src="/logo.svg" // Route of the image file
           width={216}
@@ -15,13 +17,21 @@ export default function NavMenu() {
       </Link>
       <ul className={styles.links}>
         <li>
-          <Link href={'/about'}>About</Link>
+          <Link href={"/about"}>About</Link>
         </li>
         <li>
-          <Link href={'/blog'}>Blog</Link>
+          <Link href={"/blog"}>Blog</Link>
         </li>
         <li>
-          <Link href={'/users'}>Users</Link>
+          <Link href={"/users"}>Users</Link>
+        </li>
+        <li>
+          <SignInButton />
+        </li>
+        <li>
+          <AuthCheck>
+            <SignOutButton />
+          </AuthCheck>
         </li>
       </ul>
     </nav>
