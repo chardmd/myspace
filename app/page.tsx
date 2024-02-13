dde8ded220f3268f2224601a191e11d476902907
Richard Dimalanta <chardmd.au@gmail.com>
2024-02-13 14:10:16 +1100

    added blog

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-import { authOptions } from "./api/auth/[...nextauth]/route";
-
-export default async function Home() {
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    redirect("/api/auth/signin");
-  }
-
-  return <main className={styles.main}></main>;
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to NextSpace!</h1>
+      <p>
+        A next-gen social media app to connect with frens inspired by MySpace
+      </p>
+      <p>To get started, sign up for an account</p>
+    </div>
+  );
 }
